938e630bbec11cbdc2df7ab8e1a3606c2aa931ff
Joel Lopez <jowie.jowie@gmail.com>
2016-01-30 22:23:07 +0000

    Launch wear activity

--- wear/src/main/java/cambridge/hack/alarmbike/services/MessageListenerService.java
+++ wear/src/main/java/cambridge/hack/alarmbike/services/MessageListenerService.java
@@ -7,12 +7,19 @@ import android.util.Log;
 import com.google.android.gms.wearable.MessageEvent;
 import com.google.android.gms.wearable.WearableListenerService;
 
+import cambridge.hack.alarmbike.MapActivity;
+
 /**
  * Created by joel on 1/30/16.
  */
 public class MessageListenerService extends WearableListenerService {
     private static final String TAG = MessageListenerService.class.getSimpleName();
     public void onMessageReceived(MessageEvent messageEvent) {
+        Intent intent = new Intent(this, MapActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        startActivity(intent);
+
         String nodeId = messageEvent.getSourceNodeId();
 
         Log.d(TAG, "Connected node: " + nodeId);
@@ -25,6 +32,6 @@ public class MessageListenerService extends WearableListenerService {
         Intent messageIntent = new Intent();
         messageIntent.setAction(Intent.ACTION_SEND);
         messageIntent.putExtra("message", list);
-        LocalBroadcastManager.getInstance(this).sendBroadcast(messageIntent);
+        //LocalBroadcastManager.getInstance(this).sendBroadcast(messageIntent);
     }
 }
