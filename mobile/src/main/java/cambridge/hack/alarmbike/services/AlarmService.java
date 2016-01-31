703e82f6b56614545c8a061da7825dfbc090b170
Joel Lopez <jowie.jowie@gmail.com>
2016-01-31 07:54:07 +0000

    Alarm event fix

---
+++ mobile/src/main/java/cambridge/hack/alarmbike/services/AlarmService.java
@@ -0,0 +1,55 @@
+package cambridge.hack.alarmbike.services;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.Context;
+
+import cambridge.hack.alarmbike.callback.CreateAlarmCallback;
+import cambridge.hack.alarmbike.entities.Alarm;
+import cambridge.hack.alarmbike.entities.DateAlarm;
+import cambridge.hack.alarmbike.enums.OriginOrDestination;
+import io.realm.Realm;
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p/>
+ * TODO: Customize class - update intent actions, extra parameters and static
+ * helper methods.
+ */
+public class AlarmService extends IntentService {
+
+    public AlarmService() {
+        super("AlarmService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            int id = intent.getIntExtra("id", -1);
+            if (id > 0) {
+                DateAlarm alarm = Realm.getInstance(this).where(DateAlarm.class).equalTo("id", id).findFirst();
+                switch (intent.getAction()) {
+                    case "init":
+                        ApiAdapter.getInstance(this).createAlarm(alarm.getStation(),
+                                (alarm.getOriginOrDestination() == OriginOrDestination.ORIGIN.ordinal()) ?
+                                        OriginOrDestination.ORIGIN : OriginOrDestination.DESTINATION, new CreateAlarmCallback() {
+                            @Override
+                            public void onCreateAlarm(Alarm alarm) {
+
+                            }
+
+                            @Override
+                            public void onError(Throwable t) {
+
+                            }
+                        });
+                        break;
+                    case "end":
+                        ApiAdapter.getInstance(this).finishAlarm(alarm);
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ mobile/src/main/java/cambridge/hack/alarmbike/services/BootReceiver.java
@@ -0,0 +1,38 @@
+package cambridge.hack.alarmbike.services;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.List;
+
+import cambridge.hack.alarmbike.entities.DateAlarm;
+import io.realm.Realm;
+
+public class BootReceiver extends BroadcastReceiver {
+    public BootReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Realm realm = Realm.getInstance(context);
+        List<DateAlarm> alarms = realm.where(DateAlarm.class).findAll();
+
+        AlarmManager alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+
+        for (DateAlarm alarm : alarms) {
+            Intent initIntent = new Intent().setAction("init").putExtra("id", alarm.getId());
+            PendingIntent pending = PendingIntent.getService(context, 0, initIntent, 0);
+            alarmManager.set(AlarmManager.RTC_WAKEUP, alarm.getInitDate().getTime(), pending);
+
+            Intent finishIntent = new Intent().setAction("finish").putExtra("id", alarm.getId());
+            pending = PendingIntent.getService(context, 0, finishIntent, 0);
+            alarmManager.set(AlarmManager.RTC_WAKEUP, alarm.getFinishDate().getTime(), pending);
+        }
+
+        realm.close();
+    }
+}
